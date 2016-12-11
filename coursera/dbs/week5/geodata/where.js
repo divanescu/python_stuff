ba56926bd70bb6504887d6d15a60573cbbc26a94
Daniel Ivanescu <dani.ivanescu@gmail.com>
2016-12-11 14:05:12 +0000

    week 5 geolocation

---
+++ coursera/dbs/week5/geodata/where.js
@@ -0,0 +1,6 @@
+myData = [
+[47.6498128,-122.3037817, 'University of Washington Station, 3720 Montlake Blvd NE, Seattle, WA 98195, USA'],
+[19.3188895,-99.1843676, 'University City, Mexico City, CDMX, Mexico'],
+[42.9097484,-85.7630885, 'Grandville, MI, USA'],
+[50.1577431,8.6951428, 'An den Drei Hohen 41, 60435 Frankfurt am Main, Germany']
+];
